d325f0fe44f7ce2a93f1e292ee3a9cbfe22077e7
Carmen Chow <carmendevelops@gmail.com>
2022-11-13 12:38:24 -0500

    change background images

--- main.js
+++ main.js
@@ -1,74 +1,131 @@
 // Get DOM elements
 const key = '75e7ccabdef5725374998f0c3f3798b2';
-const cityInput = document.getElementById('search-query'); //city
+const cityInput = document.getElementById('search-query'); 
 const submitBtn = document.querySelector('.search');
-const displayWeather = document.querySelector('.search-results'); //results
-const displayWeek = document.querySelector('.weekly-forecast'); //results
+const displayWeather = document.querySelector('.search-results'); 
+const displayWeek = document.querySelector('.weekly-forecast'); 
 const setDefault = document.getElementById('default');
+
+const background = {
+  "01d": "images/clear.jpg", //San Francisco
+  "01n": "images/clear.jpg",
+  "02d": "images/few-clouds.jpg",
+  "02n": "images/few-clouds.jpg",
+  "03d": "images/scattered-clouds.jpg", // Miami: scattered clouds
+  "03n": "images/scattered-clouds.jpg",
+  "04d": "images/overcast.jpg", // Toronto: overcast clouds
+  "04n": "images/overcast.jpg",
+  "09d": "images/rainy.jpg",
+  "09d": "images/rainy.jpg",
+  "10d": "images/rainy.jpg", // MOntreal, Orlando: light rain;
+  "10n": "images/rainy.jpg",
+  "11d": "images/thunderstorm.jpg",
+  "11n": "images/thunderstorm.jpg",
+  "13d": "images/snow.jpg", //Yellowknife
+  "13n": "images/snow.jpg",  
+  "50d": "images/mist.jpg",
+  "50n": "images/mist.jpg", // London: mist
+}
+
 
 // Default City
 // window.location.reload();
 // setDefault.addEventListener('click', function(){
 //   document.getElementById("cityInput").defaultValue = cityInput;
 //   GetInfo();
-
   // document.getElementById("myText").defaultValue = "Goofy";
-
 // })
 
 
-// Icons object 
-const weatherIcons = ["01d", "01n", "02d", "02n", "03d", "03n", "04d", "04n", "05d", "05n", "06d", "06n", "07d", "06n", "07d", "07n", "08d", "08n", "09d", "09n", "10d", "10n", "11d", "11n", "12d", "12n", "13d", "13n", "50d", "50n"]
-
-const backgroundImages = ["images/sunny.jpg", "slides-cloudy.jpg",
-"slides-rainy.jpg", "slides-thunderstorm.jpg", "slides-snow.jpg", 
-"slides-mist.jpg" ]
-
-
-const background = {
-  // "01d": "images/
-  // "01n":
-}
-
-// 'clear': 01n.png, 01.png,
-// 'few clouds': 02d.png, 02n.png,
-// 'scatterec clouds': 03d.png, 03n.png,
-// 'broken clouds': 04d.png, 04n.png,
-// 'shower rain': 09d.png, 09n.png,
-// 'rain': 10d.png, 10n.png,
-// 'thunderstorm': 11d.png, 11n.png,
-// 'snow': 13d.png, 13n.png,
-// 'mist': 50d.png, 50n.png
-
-// <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-
-document.body.style.backgroundImage = "url('images/sunny.jpg')";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "cover";
-
 // Part 1: Function to fetch current weather details from API and display results:
+
+submitBtn.addEventListener('keypress', getCurrentWeather){
+  if(e.key === "Enter"){
+
+    
+  }
+};
+ 
+
+
 const getCurrentWeather = function(){
   var cityValue = cityInput.value;
   console.log(`searching for city of ${cityValue}`)
+
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=imperial`)
     .then((response) => response.json())
     .then((data) => {
-// Change Background Image Weather
-  if(data.weather[0].icon === '04n' || data.weather[0].icon === '04n'){
-      document.body.style.backgroundImage = "url('images/mist.jpg')";
-      document.body.style.backgroundRepeat = "no-repeat";
-      ddocument.body.style.backgroundSize = "cover";
-  }
-  else if (data.weather[0].icon === '10d' || data.weather[0].icon === '10n'){
-    document.body.style.backgroundImage = "url('images/rainy.jpg')";
-    document.body.style.backgroundRepeat = "no-repeat";
-    ddocument.body.style.backgroundSize = "cover";
-  }
 
+    // Switch statements to change background images
+    const backgroundImage = data.weather[0].icon;
+    console.log('Weather icon code = ' + backgroundImage);
+    console.log('Background image =' + background["50n"]);
+
+    switch(backgroundImage){
+      case "01d": 
+        document.body.style.backgroundImage = "url('images/clear.jpg')";
+        break;
+      case "01n": 
+        document.body.style.backgroundImage = "url('images/clear.jpg')";
+        break;
+      case "02d":
+        document.body.style.backgroundImage = "url('images/few-clouds.jpg')";
+        break;
+      case "02n":
+        document.body.style.backgroundImage = "url('images/few-clouds.jpg')";
+        break;
+      case "03d": 
+        document.body.style.backgroundImage = "url('images/scattered-clouds.jpg')";
+        break;
+      case "03n": 
+        document.body.style.backgroundImage = "url('images/scattered-clouds.jpg')";
+      case "04d":
+        document.body.style.backgroundImage = "url('images/overcast.jpg')";
+        break;
+      case "04n": 
+        document.body.style.backgroundImage = "url('images/overcast.jpg')";
+        break;
+      case "09d":
+        document.body.style.backgroundImage = "url('images/rainy.jpg')";
+        break;
+      case "09n": 
+        document.body.style.backgroundImage = "url('images/rainy.jpg')";
+        break;
+      case "10d":
+        document.body.style.backgroundImage = "url('images/rainy.jpg')";
+        break;
+      case "10n":
+        document.body.style.backgroundImage = "url('images/rainy.jpg')";
+        break;
+      case "11d":
+        document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+        break;
+      case "11n":
+        document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+        break;
+      case "13d":
+        document.body.style.backgroundImage = "url('images/snow.jpg')";
+        break;
+      case "13n":
+        document.body.style.backgroundImage = "url('images/snow.jpg')";
+        break;
+      case "50d":
+        document.body.style.backgroundImage = "url('images/mist.jpg')";
+        break;
+      case "50n":
+        document.body.style.backgroundImage = "url('images/mist.jpg')";
+        break;
+    }
+
+
+    // document.body.style.backgroundImage = "url('images/snow.jpg')";     
+   // document.body.style.backgroundImage = "url('background['11d']'";     
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";  
 
   displayWeather.innerHTML = `
     <div className="weather-row">
-    
+
       <div>
         <h2 className="weekly-temp">${data.main.temp.toFixed(0)}°</h2>
         <h4>${data.name}</h4>
@@ -79,9 +136,7 @@ const getCurrentWeather = function(){
       </div>
     </div>
   `
-})
-
-
+  })
 
 // Part 2: Function to fetch 5 day weather details from API and display results:
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=${key}&units=imperial`)
@@ -126,5 +181,3 @@ for(i = 0; i<5; i++){
 
 
 submitBtn.addEventListener('click', getCurrentWeather);
-
- 
